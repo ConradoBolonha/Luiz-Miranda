926caae0c32bcf0e4dd04132619d7ff8a86c065a
Conrado <conradobolonha@gmail.com>
2022-08-02 16:58:00 -0300

    v.1.00

---
+++ Aula01/Aula01-1.py
@@ -0,0 +1,11 @@
+#  print('Linha 1')
+print('Linha 2') #  Vai imprimir "Linha 2" na tela.
+print('Linha 3')
+print('Linha 4')
+'''
+print('Linha 5')
+print('Linha 6')
+print('Linha 7')
+print('Linha 8')
+'''
+print(123456)
