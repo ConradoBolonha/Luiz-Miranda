7054cae66152db0e072d46fc44ac2165502c6408
Conrado <conradobolonha@gmail.com>
2022-08-04 17:17:42 -0300

    v.1.00

--- Aula03/aula03-01.py
+++ Aula03/aula03-01.py
@@ -1,3 +1,4 @@
+'''
 print('Hello World!') #  string = texto
 print("Hello World!") #  string = texto
 print(123456)
@@ -6,3 +7,5 @@ print('Essa é uma string (str)')
 print('Essa é uma string (str)')
 print('Essa é uma {}'.format('string'))
 print('Esse é um \'texto\' de teste.')
+'''
+print(r'Colocar R exibe o /n também')
---
+++ Aula04/aula04-1.py
@@ -0,0 +1,12 @@
+'''
+print(type('Conrado'))
+
+print(type(2049))
+
+print(type(23.67))
+
+print(type(False))
+print(type(True))
+'''
+
+print (10 == 10, type('10 == 10'))
