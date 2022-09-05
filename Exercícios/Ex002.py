1d3af0f791a7ab61b77e4c75b7dd291393122533
Conrado <conradobolonha@gmail.com>
2022-09-05 17:47:52 -0300

    v.1.00

--- Exercícios/Ex002.py
+++ Exercícios/Ex002.py
@@ -1,6 +1,15 @@
 '''
 Faça um programa que peça ao usuário para digitar um número inteiro,
-informe se este número é par ou ímpar. CAso o usuário não digite um
+informe se este número é par ou ímpar. Caso o usuário não digite um
 número inteiro, informe que não é um número inteiro.
 '''
 
+num = input('Digite um número: ')
+
+if num.isnumeric():
+    num = int(num)
+    if num % 2 ==0:
+        print(f'Você digitou o número {num} e esté é "PAR".')
+    else: print(f'Você digitou o número {num} e este é "ÍMPAR".')
+else:
+    print ('Digite apenas números inteiros!')
--- Exercícios/Ex003.py
+++ Exercícios/Ex003.py
@@ -1,6 +1,17 @@
 '''
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário
-descrito, exiba a saudação apropriada. Exemplo: Das 0s as 11h59, bom dia.
-Das 12hs as 17hs, boa tarde e das 17h01 as 23h59, boa noite.
+descrito, exiba a saudação apropriada. Exemplo: Das 0h as 12h, bom dia.
+Das 13h as 17h, boa tarde e das 18h as 23h, boa noite.
 '''
 
+hora = input('Que horas são? ')
+if hora.isnumeric():
+    hora = int(hora)
+    if hora > 0 and hora < 12:
+        print('Bom dia!')
+    elif hora > 13 and hora < 17:
+        print('Boa tarde!')
+    elif hora > 18 and hora < 23:
+        print('Boa noite!')
+else:
+    print('Digite apenas números inteiros!')
