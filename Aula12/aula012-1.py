7900c3a6755b24ba89da039e499590942b495bea
Conrado <conradobolonha@gmail.com>
2022-09-06 16:08:21 -0300

    v.1.00

--- Aula12/aula012-1.py
+++ Aula12/aula012-1.py
@@ -32,9 +32,6 @@ print(f'{div:.2f}')
 num_3 = 199
 print(f'{num_3:0>10}') #  Acrescenta sete zeros a esquerda
 print(f'{num_3:0<10}') #  Acrescenta sete zeros a direta
-<<<<<<< HEAD
 print(f'{num_3:0^10}') #  Coloca o número 10 no centro
 print(f'{num_3:.2f}') #  Tranforma o número inteiro 199 em float (199.00)
 print(f'{num_3:0<10.2f}')
-=======
->>>>>>> 89b7f2a9beda540b2c7ebe13c4218f1c977bc982
